055372c2b47dbc86eeb7b635af450b323c03803f
tajbaba999 <tajbaba153@gmail.com>
2024-10-21 22:08:20 +0530

    📌 Sigin + Signup

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -29,6 +31,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <div className="header">
+            <UserButton />
+          </div>
           {children}
         </body>
       </html>
